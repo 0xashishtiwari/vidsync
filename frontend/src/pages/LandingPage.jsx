a4eff9f48bc3121e06640129e95348ac51bf58ca
0xashishtiwari <ashishinrewa@gmail.com>
2025-06-20 22:33:40 +0530

    created the landing page

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../App.css";
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
   return (
@@ -11,11 +12,24 @@ const LandingPage = () => {
         <div className="navlist">
           <p>Join as Guest</p>
           <p>Register</p>
-          <div role="button">
+          <div role='button'>
             <p>Login</p>
           </div>
         </div>
       </nav>
+      <div className="landingMainContainer">
+        <div>
+          <h1><span style={{color : "#FF9839"}}>Connect</span> with your loved Ones </h1>
+
+          <p>Cover a distance by vidsync</p>
+          <div role='button'>
+            <Link to={'/home'}> Get Started </Link>
+          </div>
+        </div>
+        <div >
+          <img src="/mobile.png" alt="" />
+        </div>
+      </div>
     </div>
   );
 };
